f76567ad5a6ab380233c9620b173eabbd76b4102
ghaith <mlika.gaith@gmail.com>
2022-03-12 00:50:51 +0100

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
