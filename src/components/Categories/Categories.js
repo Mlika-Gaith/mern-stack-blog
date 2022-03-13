8469831555ec912e9d83d5683f805fd004bb9f96
ghaith <mlika.gaith@gmail.com>
2022-03-13 18:07:40 +0100

    added a categories component and reduced complexity.

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,56 @@
+import "./Categories.css";
+function Categories() {
+  return (
+    <ul>
+      <li>
+        <input type="checkbox" id="checkboxOne" value="tech" />
+        <label htmlFor="checkboxOne">Tech</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxTwo" value="sports" />
+        <label htmlFor="checkboxTwo">Sports</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxThree" value="culture" />
+        <label htmlFor="checkboxThree">Culture</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxFour" value="science" />
+        <label htmlFor="checkboxFour">Science</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxFive" value="student life" />
+        <label htmlFor="checkboxFive">Student life</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxSix" value="entertainement" />
+        <label htmlFor="checkboxSix">Entertainement</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxSeven" value="certifications" />
+        <label htmlFor="checkboxSeven">Certifications</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxEight" value="Camping" />
+        <label htmlFor="checkboxEight">Camping</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxNine" value="social media" />
+        <label htmlFor="checkboxNine">Social media</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxTen" value="leadership" />
+        <label htmlFor="checkboxTen">Leadership</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxEleven" value="arts" />
+        <label htmlFor="checkboxEleven">Arts</label>
+      </li>
+      <li>
+        <input type="checkbox" id="checkboxTwelve" value="academic" />
+        <label htmlFor="checkboxTwelve">Academic</label>
+      </li>
+    </ul>
+  );
+}
+export default Categories;
