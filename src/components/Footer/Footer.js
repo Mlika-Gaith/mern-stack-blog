c02d3a861a6356dffdd21b8301cf6a114e3aa9c4
ghaith <mlika.gaith@gmail.com>
2022-03-14 11:18:58 +0100

    added footer component.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+function Footer() {
+  return (
+    <div className={styles.container}>
+      <p>
+        Created with <i className="fa-solid fa-heart"></i> by ghaith mlika
+      </p>
+    </div>
+  );
+}
+export default Footer;
