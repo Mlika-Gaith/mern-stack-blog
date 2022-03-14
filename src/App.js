0475676f985665b499a31a269e3119fc4ba97092
ghaith <mlika.gaith@gmail.com>
2022-03-14 11:17:33 +0100

    added footer.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import NavBar from "./components/Navbar/Navbar";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Register />
+      <Footer />
     </div>
   );
 }
