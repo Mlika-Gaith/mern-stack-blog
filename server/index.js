96a9dd25441c5c017d284fee8878588cea0c4cc2
ghaith <mlika.gaith@gmail.com>
2022-03-17 23:33:16 +0100

    add MongoDB connection.

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// * creating express server
+// * install and use nodemon for a responsive server
+const express = require("express");
+const app = express();
+// * adding mongodb url
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(console.log("connected to db"));
+app.listen("8080", () => console.log("server running"));
